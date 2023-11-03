ee27bdb686a96e6965e0c6c3b0410cfded314072
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-11-03 10:56:52 +0000

    add

---
+++ practice-2/practice-2-19.py
@@ -0,0 +1,17 @@
+# 問題2-18: 誤発注
+
+input_line = 5
+input_data = [1,4,6,2,1]
+products = {}
+for i in range(1,input_line+1):
+    products[i] = products.get(i, 0)
+    
+for k in range(input_line):
+    product = input_data[k]
+    products[product] = products.get(product, 0) + 1
+
+count = 0
+for v in products.values():
+    if v == 0:
+        count += 1
+print(count)
