248ed1c071056ed97ce7f56cb4cd42e2e9d6b7be
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-11-08 15:18:56 +0000

    add

--- practice-3/practice-3-7.py
+++ practice-3/practice-3-7.py
@@ -1,33 +1,36 @@
-#問題3-7: 
+#問題3-7: ゴンドラ
 
-def ride(gondola,people):
+def ride(gondola, people):
     if gondola >= people:
         return people, 0
     else:
-        people -= gondola
-        return gondola, people
-        
-gondola, group = map(int, input().split())
+        return gondola, people - gondola
 
-gondolas = []
-result = []
-for i in range(gondola):
-    gondolas.append(int(input()))
-    result.append(int(0))
-
+gondola, group = 3,3
+gondolas = [5,5,5]
+result = [0,0,0]
 peoples = []
+people_data = [6,5,3]
 for i in range(group):
-    peoples.append(int(input()))
+    peoples.append(people_data[i])
 
 k = 0
+
 for people in peoples:
-    value, left = ride(gondolas[k],people)
+    value, left = ride(gondolas[k], people)
     result[k] += value
-    if k+1 >= gondola:
-        K = 0
-    else:
+    left_people = left
+
+    while left_people > 0:
         k += 1
-    result[k] += left
+        if k >= gondola:
+            k = 0
+        value, left = ride(gondolas[k], left_people)
+        result[k] += value
+        left_people = left
+    k += 1
+    if k >= gondola:
+        k = 0
 
 for i in result:
     print(i)
