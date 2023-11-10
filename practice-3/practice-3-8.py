8442502ed2c34d597dee7d6dc296c1178e79a38d
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-11-10 14:21:54 +0000

    add

--- practice-3/practice-3-8.py
+++ practice-3/practice-3-8.py
@@ -1,13 +1,21 @@
-#問題3-8: 
+#問題3-8: 暴風域にいますか？
+
+def check_position(xc, yc, r_1, r_2, x, y):
+    distance_to_origin = (x - xc) ** 2 + (y - yc) ** 2
+    r_1_squared = r_1 ** 2
+    r_2_squared = r_2 ** 2
+
+    if r_1_squared <= distance_to_origin <= r_2_squared:
+        return True
+    else:
+        return False
 
 xc, yc, r_1, r_2 = map(int, input().split())
 cases = int(input())
 
 for i in range(cases):
     x, y = map(int, input().split())
-    x = abs(x)
-    y = abs(y)
-    if r_1 * 2 <= (x - xc) * 2 and r_2 * 2 >= (x - xc) * 2 and r_2 * 2 >= (y - yc):
+    if check_position(xc, yc, r_1, r_2, x, y):
         print("yes")
     else:
         print("no")
